85d6edf7962db148921f6ddad620709ea22aa85b
Bruno Azisaka <bruno@azisaka.com.br>
2015-01-13 16:05:21 -0200

    Version bump

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,7 @@ maintainer_email "contact@bubble.com.br"
 license          "MIT"
 description      "Installs/Configures Brightbox's Ruby"
 long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
-version          "0.1.1"
+version          "0.1.2"
 
 depends          "apt"
+depends          "build-essential"
